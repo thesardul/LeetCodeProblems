d1748dbc30c5f39f91aae155757ab7c142e1505d
Sardul Yadav <27910681+thesardul@users.noreply.github.com>
2022-07-26 22:35:36 +0530

    Update InOrderTraversal.java
    Added code for Inorder Traversal

--- src/com/thesardul/tree/InOrderTraversal.java
+++ src/com/thesardul/tree/InOrderTraversal.java
@@ -1,2 +1,19 @@
-package com.thesardul.tree;public class InOrderTraversal {
+package com.thesardul.tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InOrderTraversal {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        final List<Integer> res = new ArrayList<>();
+        inorderTraversal(root, res);
+        return res;
+    }
+    private void inorderTraversal(final TreeNode root1, final List<Integer> res){
+        if(root1 != null){
+            inorderTraversal(root1.left, res);
+            res.add(root1.val);
+            inorderTraversal(root1.right, res);
+        }
+    }
 }
